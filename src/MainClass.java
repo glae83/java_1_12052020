21190a15139feec65ecc8088f1c686cdf2ab8639
glae83 <glae83@gmail.com>
2020-05-15 11:26:07 +0400

    homework

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,43 +1,55 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
 
-    byte a = 10;
-    short b = 1232;
-    int c = 1000;
-    long d = 300000L;
-    float e = 15.57f;
-    double g = 256.587;
-    char k = '\u2565';
-    boolean s = true;
+        byte a = 10;
+        short b = 1232;
+        int c = 1000;
+        long d = 300000L;
+        float e = 15.57f;
+        double g = 256.587;
+        char k = '\u2565';
+        boolean s = true;
 
-
-        System.out.println(Account(4,8,9,15));
-        System.out.println(Summ(5.8,20));
-        Number(-5);
-        System.out.println(Numder2(-10));
+        System.out.println(account(4,8,9,15));
+        System.out.println(summ(5.8,20));
+        number(-5);
+        System.out.println(numder2(-5));
         printHello("Фанзиль");
-           }
-
-public static double Account(double a, double b, double c, double d){
+        year(204);
+    }
+    // Задание 3
+    public static double account(double a, double b, double c, double d){
         return a*(b+(c/d));
-}
-
-public static boolean Summ(double a, double b){
+    }
+    // Задание 4
+    public static boolean summ(double a, double b){
         return (a+b)>=10 && (a+b)<=20;
-}
-
-public static void  Number(int a){
+    }
+    // Задание 5
+    public static void  number(int a){
         if (a<0) {
             System.out.println("Отрицательное");
         } else {
             System.out.println("Положительное");
-    }}
-public static boolean Numder2(int b){
+        }}
+    // Задание 6
+    public static boolean numder2(int b){
         return b<0;
-}
-
-public static void printHello(String hello){
-    System.out.println("Привет,"+ hello);
-}
+    }
+    // Задание 7
+    public static void printHello(String hello){
+        System.out.println("Привет,"+ hello);
+    }
+    // Задание 8
+    public static void year(int a){
+        if (a % 4 == 0 && a % 400 == 0) {
+            System.out.println("Високосный");
+        }
+        if (a % 4 == 0 && a < 100 ) {
+            System.out.println("Високосный");
+        }
+        if (a % 4 == 0 && a % 100 != 0 ) {
+            System.out.println("Високосный");
+        }
+    }
 }
